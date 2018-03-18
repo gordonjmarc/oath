c3d9d30fa4d7483a4963f2a936eaba19312c2087
Marc Gordon <mjg31337@gmail.com>
2018-03-18 14:25:58 -0400

    completed all scraping regex/logic, will refactor next

--- src/LogScraper.php
+++ src/LogScraper.php
@@ -24,25 +24,32 @@ class LogScraper
     
 	public function parseEntries() 
 	{
-        //$pattern=  "/# User@Host: /"; 
-		$pattern = "/# ;/";
+        $pattern=  "/# User@Host: /"; 
 
- 		$queryTimePattern = "/Query_time: (([0-9]|\.)*)/";
+ 		//$hostPattern = "/ .*@ (.*) Id:/";
+        $queryTimePattern = "/Query_time: (([0-9]|\.)*)/";
  		$lockTimePattern = "/Query_time: (([0-9]|\.)*)/";
  		$rowsSentPattern = "/Rows_sent: (([0-9])*)/";
+        $bytesSentPattern = "/Bytes_sent: (([0-9])*)/";
+        $rowsExaminedPattern = "/Rows_examined: (([0-9])*)/";
+        $rowsEffectedPattern = "/Rows_affected: (([0-9])*)/";
+        $timeStampPattern = "/SET timestamp=(([0-9])*)/";
 
         $entries = preg_split($pattern, $this->contents, -1, PREG_SPLIT_NO_EMPTY);
 
         foreach($entries as $entry) 
         {
-        	$user= "";
-        	$host ="";
-        	$query="";
         	$queryTime = "";
         	$lockTime = "";
         	$rowsSent = "";
+            $bytesSent = "";
+            $rowsExamined = "";
+            $rowsEffected = "";
+            $timeStamp = "";
+            $query="";
+            $queryType = "";
 
-        	/*
+        	
 	 		if(preg_match($queryTimePattern, $entry, $matches) ) 
 			{
 				$queryTime= $matches[1];
@@ -56,7 +63,43 @@ class LogScraper
 			if(preg_match($rowsSentPattern, $entry, $matches) ) 
 			{
 				$rowsSent = $matches[1];
-			}*/
+			}
+
+            if(preg_match($bytesSentPattern, $entry, $matches) ) 
+            {
+                $bytesSent = $matches[1];
+            }
+
+            if(preg_match($rowsExaminedPattern, $entry, $matches) ) 
+            {
+                $rowsExamined = $matches[1];
+            }
+
+            if(preg_match($rowsEffectedPattern, $entry, $matches) ) 
+            {
+                $rowsEffected = $matches[1];
+            }
+
+            if(preg_match($timeStampPattern, $entry, $matches) ) 
+            {
+                $timeStamp = $matches[1];
+            }
+
+            if(stripos($entry, ';') > 0) //query exists between semi colons
+            {
+                $query = substr($entry, stripos($entry, ';'));
+                $queryType = substr($query, 0 , stripos($query, ' '));
+
+            } 
+
+     
+
+            //echo "\n";
+            //var_dump($timeStamp);
+           // var_dump($query);
+          //  var_dump($queryType);
+
+          //  die();
      }
 
 
@@ -66,7 +109,7 @@ class LogScraper
        // var_dump($entries[0]);
         //echo "\n";
        
-        var_dump($entries[1]);
+        //var_dump($entries[1]);
        // var_dump($entries[count($entries) -1 ]    );
 
 
