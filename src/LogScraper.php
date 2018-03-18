020150085fbfe2b3eb4bf5e545329086c8032207
Marc Gordon <mjg31337@gmail.com>
2018-03-18 14:40:49 -0400

    white space cleanup, adding additional args for ts filtering TBD later

--- src/LogScraper.php
+++ src/LogScraper.php
@@ -4,130 +4,129 @@
  */
 
 
-
 class LogScraper
 {
 
-	private $file;
-	private $contents;
+	protected $file;
+	protected $contents;
 
-    public function __construct($file)
-    {
-        if (!file_exists($file)) {
-            throw new FileNotFoundException("File not found");
-        }
+	protected $entryPattern=  "/# User@Host: /"; 
+
+	protected $queryTimePattern = "/Query_time: (([0-9]|\.)*)/";
+	protected $lockTimePattern = "/Query_time: (([0-9]|\.)*)/";
+	protected $rowsSentPattern = "/Rows_sent: (([0-9])*)/";
+	protected $bytesSentPattern = "/Bytes_sent: (([0-9])*)/";
+	protected $rowsExaminedPattern = "/Rows_examined: (([0-9])*)/";
+	protected $rowsEffectedPattern = "/Rows_affected: (([0-9])*)/";
+	protected $timeStampPattern = "/SET timestamp=(([0-9])*)/";
+
+
+	public function __construct($file)
+	{
+		if (!file_exists($file)) 
+		{
+			throw new FileNotFoundException("File not found");
+		}
 
 		$this->file = new \SplFileInfo($file);
-        $contents = $this->file->openFile("r");
-        $this->contents = $contents->fread($this->file->getSize());
-    }
-    
+		$contents = $this->file->openFile("r");
+		$this->contents = $contents->fread($this->file->getSize());
+	}
+
 	public function parseEntries() 
 	{
-        $pattern=  "/# User@Host: /"; 
 
- 		//$hostPattern = "/ .*@ (.*) Id:/";
-        $queryTimePattern = "/Query_time: (([0-9]|\.)*)/";
- 		$lockTimePattern = "/Query_time: (([0-9]|\.)*)/";
- 		$rowsSentPattern = "/Rows_sent: (([0-9])*)/";
-        $bytesSentPattern = "/Bytes_sent: (([0-9])*)/";
-        $rowsExaminedPattern = "/Rows_examined: (([0-9])*)/";
-        $rowsEffectedPattern = "/Rows_affected: (([0-9])*)/";
-        $timeStampPattern = "/SET timestamp=(([0-9])*)/";
+		$entries = preg_split($this->entryPattern, $this->contents, -1, PREG_SPLIT_NO_EMPTY);
 
-        $entries = preg_split($pattern, $this->contents, -1, PREG_SPLIT_NO_EMPTY);
+		foreach($entries as $entry) 
+		{
+			$queryTime = "";
+			$lockTime = "";
+			$rowsSent = "";
+			$bytesSent = "";
+			$rowsExamined = "";
+			$rowsEffected = "";
+			$timeStamp = "";
+			$query="";
+			$queryType = "";
 
-        foreach($entries as $entry) 
-        {
-        	$queryTime = "";
-        	$lockTime = "";
-        	$rowsSent = "";
-            $bytesSent = "";
-            $rowsExamined = "";
-            $rowsEffected = "";
-            $timeStamp = "";
-            $query="";
-            $queryType = "";
-
-        	
-	 		if(preg_match($queryTimePattern, $entry, $matches) ) 
+	 		if(preg_match($this->queryTimePattern, $entry, $matches) ) 
 			{
 				$queryTime= $matches[1];
 			}
 			
-			if(preg_match($lockTimePattern, $entry, $matches) ) 
+			if(preg_match($this->lockTimePattern, $entry, $matches) ) 
 			{
 				$lockTime = $matches[1];
 			}
 
-			if(preg_match($rowsSentPattern, $entry, $matches) ) 
+			if(preg_match($this->rowsSentPattern, $entry, $matches) ) 
 			{
 				$rowsSent = $matches[1];
 			}
 
-            if(preg_match($bytesSentPattern, $entry, $matches) ) 
-            {
-                $bytesSent = $matches[1];
-            }
+			if(preg_match($this->bytesSentPattern, $entry, $matches) ) 
+			{
+				$bytesSent = $matches[1];
+			}
 
-            if(preg_match($rowsExaminedPattern, $entry, $matches) ) 
-            {
-                $rowsExamined = $matches[1];
-            }
+			if(preg_match($this->rowsExaminedPattern, $entry, $matches) ) 
+			{
+				$rowsExamined = $matches[1];
+			}
 
-            if(preg_match($rowsEffectedPattern, $entry, $matches) ) 
-            {
-                $rowsEffected = $matches[1];
-            }
+			if(preg_match($this->rowsEffectedPattern, $entry, $matches) ) 
+			{
+				$rowsEffected = $matches[1];
+			}
 
-            if(preg_match($timeStampPattern, $entry, $matches) ) 
-            {
-                $timeStamp = $matches[1];
-            }
+			if(preg_match($this->timeStampPattern, $entry, $matches) ) 
+			{
+				$timeStamp = $matches[1];
+			}
 
-            if(stripos($entry, ';') > 0) //query exists between semi colons
-            {
-                $query = substr($entry, stripos($entry, ';'));
-                $queryType = substr($query, 0 , stripos($query, ' '));
-
-            } 
-
-     
-
-            //echo "\n";
-            //var_dump($timeStamp);
-           // var_dump($query);
-          //  var_dump($queryType);
-
-          //  die();
-     }
-
-
-        
-       // var_dump( count( $entries) );
-       
-       // var_dump($entries[0]);
-        //echo "\n";
-       
-        //var_dump($entries[1]);
-       // var_dump($entries[count($entries) -1 ]    );
-
-
-	}    	
+			if(stripos($entry, ';') > 0) //query exists between semi colons
+			{
+				$query = substr($entry, stripos($entry, ';'));
+				$queryType = substr($query, 0 , stripos($query, ' '));
+			} 
+	}
 
 
 }
 
-$file = $argv[1];
+
+}
+
+$file = "";
+$startTime = "";
+$endTime = ""; 
+
+if(!empty($argv[1]))
+{
+	$file = $argv[1];
+}
+
+if(!empty($argv[2]))
+{
+	$startTime = $argv[2];
+}
+
+if(!empty($argv[3]))
+{
+	$endTime = $argv[3];
+}
+
 
 if(empty($file)) 
 {
 	throw new InvalidArgumentException("arg 1 must be a file");
 }
 
+//TODO: handle timestamp inputs
+
 $ls = new LogScraper($file);
 $ls->parseEntries();
-
 
 
 
